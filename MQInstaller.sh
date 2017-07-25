b1f0be8cf2843cf91fec89d5e72dd82bbbbd5455
michael moriarty <michael.moriarty@maersk.com>
2017-07-25 08:40:56 +0000

    new mq files

---
+++ MQInstaller.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+####################################################################################
+## 
+## SCRIPT: MQInstaller.sh
+##
+## This script will run all the other scripts 
+##
+###################################################################################
+##
+## Version  Date      Author      Description of change
+## -------  --------  ----------  ------------------------------------------------
+## 0.1      06072017  mmo275      initial version
+##
+###################################################################################
+
+prog=MQInstaller
+
+###################################################################################
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCR=$(basename "${BASH_SOURCE[0]}")
+THIS_SCRIPT=$DIR/$SCR
+LOG_DIR=/var/log/mqInstaller
+LOG_FILE=${LOG_DIR}/${prog}.log.$(date +%Y%m%d_%H%M%S_%N)
+#
+###################################################################################
+#
+# Log output to a file
+#
+###################################################################################
+function Log() {
+    if [[ ! -d ${LOG_DIR} ]]; then
+       echo "${LOG_DIR} is missing - creating ${LOG_DIR}"
+       mkdir ${LOG_DIR} 
+    fi
+    
+    if [[ -e ${LOG_DIR} ]]; then
+        echo "$(date +%Y/%m/%d_%H:%M:%S.%N) $1" >> ${LOG_FILE}
+    fi
+
+}
+
+###########################################################################
+#
+# Main section
+#
+###########################################################################
+    echo "${prog} LOG_FILE=${LOG_FILE}"
+    #
+    # This script must run under root
+    #
+    if (( $EUID != 0 ))
+    then
+         echo "${THIS_SCRIPT} must run as root"
+         Log "${THIS_SCRIPT} must run as root"
+         exit -1
+    fi
+    #
+    # 
+    #
+    
+    #
+    Log "MQInstaller complete - please check logs in ${LOG_FILE}"
+    exit 0
+
